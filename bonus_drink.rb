f86e78ea9c91d8bf2b7dbf81f1e6d97ec5df12ff
Keisuke Ishizawa <muddyapesjm66@gmail.com>
2016-04-26 21:37:22 +0900

    Add answer

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,11 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    bonus = bonus(amount)
+    amount + bonus + ( amount % 3 + bonus % 3 ) / 3
+  end
+
+  def self.bonus(amount)
+    div = amount / 3
+    div > 0 ? div + bonus(div) : 0
   end
 end
